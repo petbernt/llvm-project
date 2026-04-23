f65727ed9437e51458495e6a39fcc89ddcb18a49
Petter Berntsson <petter.berntsson@arm.com>
2026-04-23 15:32:01 +0100

    [libc] Add behavior mapping for strrchr

--- libc/test/src/string/strrchr_test.cpp
+++ libc/test/src/string/strrchr_test.cpp
@@ -11,4 +11,33 @@
 #include "src/string/strrchr.h"
 #include "test/UnitTest/Test.h"
 
-STRRCHR_TEST(Strrchr, LIBC_NAMESPACE::strrchr)
+using LlvmLibcStrrchrTest = StrrchrTest<LIBC_NAMESPACE::strrchr>;
+
+// @verifies string.strrchr.B1
+TEST_F(LlvmLibcStrrchrTest, FindsFirstCharacter) { findsFirstCharacter(); }
+
+TEST_F(LlvmLibcStrrchrTest, FindsMiddleCharacter) { findsMiddleCharacter(); }
+
+TEST_F(LlvmLibcStrrchrTest, FindsLastCharacterThatIsNotNullTerminator) {
+  findsLastCharacterThatIsNotNullTerminator();
+}
+
+// @verifies string.strrchr.B2
+TEST_F(LlvmLibcStrrchrTest, FindsNullTerminator) { findsNullTerminator(); }
+
+TEST_F(LlvmLibcStrrchrTest, FindsLastBehindFirstNullTerminator) {
+  findsLastBehindFirstNullTerminator();
+}
+
+// @verifies string.strrchr.B3
+TEST_F(LlvmLibcStrrchrTest, CharacterNotWithinStringShouldReturnNullptr) {
+  characterNotWithinStringShouldReturnNullptr();
+}
+
+TEST_F(LlvmLibcStrrchrTest, ShouldFindLastOfDuplicates) {
+  shouldFindLastOfDuplicates();
+}
+
+TEST_F(LlvmLibcStrrchrTest, EmptyStringShouldOnlyMatchNullTerminator) {
+  emptyStringShouldOnlyMatchNullTerminator();
+}
