1944117b348718680c73e61f63389e44441fb8f7
Petter Berntsson <petter.berntsson@arm.com>
2026-04-23 15:16:10 +0100

    [libc] Add behavior mapping for strlen

--- libc/test/src/string/strlen_test.cpp
+++ libc/test/src/string/strlen_test.cpp
@@ -7,8 +7,10 @@
 //===----------------------------------------------------------------------===//
 
 #include "src/string/strlen.h"
+#include "hdr/signal_macros.h"
 #include "test/UnitTest/Test.h"
 
+// @verifies string.strlen.B1
 TEST(LlvmLibcStrLenTest, EmptyString) {
   const char *empty = "";
 
@@ -16,6 +18,7 @@ TEST(LlvmLibcStrLenTest, EmptyString) {
   ASSERT_EQ((size_t)0, result);
 }
 
+// @verifies string.strlen.B2
 TEST(LlvmLibcStrLenTest, AnyString) {
   const char *any = "Hello World!";
 
@@ -23,6 +26,7 @@ TEST(LlvmLibcStrLenTest, AnyString) {
   ASSERT_EQ((size_t)12, result);
 }
 
+// @verifies string.strlen.B3
 TEST(LlvmLibcStrLenTest, DataAfterNulString) {
   constexpr char A[10] = {'a', 'b', 'c', 'd', 'e', 'f', 0, 'h', 'i', 'j'};
   size_t result = LIBC_NAMESPACE::strlen(A);
@@ -34,3 +38,12 @@ TEST(LlvmLibcStrLenTest, MultipleNulsInOneWord) {
   size_t result = LIBC_NAMESPACE::strlen(A);
   ASSERT_EQ((size_t)2, result);
 }
+
+#if defined(LIBC_ADD_NULL_CHECKS)
+
+// @verifies string.strlen.B4
+TEST(LlvmLibcStrLenTest, CrashOnNullPtr) {
+  ASSERT_DEATH([]() { (void)LIBC_NAMESPACE::strlen(nullptr); }, WITH_SIGNAL(-1));
+}
+
+#endif // defined(LIBC_ADD_NULL_CHECKS)
